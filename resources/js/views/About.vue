da94b1d16c2521d69cd845729fd0104331480dd1
Mohit Sharma <engrmohit.git@gmail.com>
2021-08-11 19:54:34 +0530

    Fix: 'About me' page
    - Change some text in 'About me' page

--- resources/js/views/About.vue
+++ resources/js/views/About.vue
@@ -8,13 +8,13 @@
                         <h2 class="heading">About me</h2>
                         <!-- <p class="lead">This can be an about section.</p> -->
                         <p class="c-text-justify">
-                            My name is <span class="c-f-w-600">Mohit Sharma</span> and I'm a <span class="c-f-w-600">full-stack software engineer</span> based in Punjab, India. 
+                            My name is <span class="c-f-w-600">Mohit Sharma</span> and I'm a <span class="c-f-w-600">full-stack web developer</span> based in Punjab, India. 
                             Since 2015 I've worked at a variety of companies. 
                             Along the way, I've also been a freelancer and building web apps for creative agencies, businesses and individuals. 
                         </p>
 
                         <p class="c-text-justify">
-                            My current stack includes PHP &amp; Laravel, Vue.js, MySQL and all the other various frameworks, 
+                            My stack includes PHP &amp; Laravel, Vue.js, MySQL and all the other various frameworks, 
                             libraries and technologies related to them. Currently I am 
                             <span class="c-f-w-600">
                                 <u><router-link to="/contact">available for hire</router-link></u>
